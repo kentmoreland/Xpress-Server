6c8d3a6d3d52199e9077a5dbd117380b9bb9561d
Kent <kentmoreland@gmail.com>
2016-12-10 18:38:53 -0600

    update: converted to es2015| added index.html| added support for client directory

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-var express = require("express"),
+const express = require("express");
+const bodyParser = require('body-parser');
 
 app = express()
 
-var port = 3000;
+const port = 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + "/client/index.html");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(`${__dirname}/client`));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.use(express.static('client'));
 
-app.listen(port, function(){
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
